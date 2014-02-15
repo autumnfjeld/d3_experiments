fc41d3ce41e41330bedb5fe04f39a3b6b592214c
Hack Reactor Students <github@hackreactor.com>
2014-02-14 18:10:54 -0800

    server edit

--- visual/express-server.js
+++ visual/express-server.js
@@ -1,16 +1,15 @@
 var express = require('express');
-var pokeServer = express();
+var server = express();
 var url = require('url');
 var path = require('path');
 var fs = require('fs');
 
-console.log('__dirname', __dirname)
+console.log('__dirname', __dirname);
 //experss.static takes a path, makes it public
-pokeServer.use(express.static(__dirname + '/client'));
+server.use(express.static(__dirname + '/client'));
 
 // pokeServer.get('/', function(req, res){
-     
 // });
 
-pokeServer.listen(8080);
+server.listen(8080);
 console.log('Listening on port 8080');
